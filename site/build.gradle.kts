69ca202ba968d87ad195870abccd87036345bcc8
Paulo Henrique Carvalho de Morais <paulohcarvalho9@gmail.com>
2024-01-11 21:37:56 -0300

    Initial commit

---
+++ site/build.gradle.kts
@@ -0,0 +1,40 @@
+import com.varabyte.kobweb.gradle.application.util.configAsKobwebApplication
+
+plugins {
+    alias(libs.plugins.kotlin.multiplatform)
+    alias(libs.plugins.jetbrains.compose)
+    alias(libs.plugins.kobweb.application)
+    // alias(libs.plugins.kobwebx.markdown)
+}
+
+group = "com.paulohc.keyproviderdemo"
+version = "1.0-SNAPSHOT"
+
+kobweb {
+    app {
+        index {
+            description.set("Powered by Kobweb")
+        }
+    }
+}
+
+kotlin {
+    configAsKobwebApplication("keyproviderdemo", includeServer = true)
+
+    sourceSets {
+        commonMain.dependencies {
+            implementation(compose.runtime)
+        }
+
+        jsMain.dependencies {
+            implementation(compose.html.core)
+            implementation(libs.kobweb.core)
+            // implementation(libs.kobweb.silk)
+            // implementation(libs.silk.icons.fa)
+            // implementation(libs.kobwebx.markdown)
+        }
+        jvmMain.dependencies {
+            compileOnly(libs.kobweb.api) // Provided by Kobweb backend at runtime
+        }
+    }
+}
